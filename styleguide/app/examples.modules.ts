30397b6a7e06bf9fb17293a6a24e478fdb696eb2
Nikolai Nossulenko <nossulenkonikolai@gmail.com>
2018-08-02 17:40:22 +0200

    solved ERROR in ./styleguide/app/examples.routes.ts

--- styleguide/app/examples.modules.ts
+++ styleguide/app/examples.modules.ts
@@ -1,10 +1,10 @@
-import { AgendaExamplesModule } from '@acpaas-ui/ngx-examples/agenda';
+
 import { AvatarExamplesModule } from '@acpaas-ui/ngx-examples/avatar';
 import { CalendarExamplesModule } from '@acpaas-ui/ngx-examples/calendar';
 import { LogoExamplesModule } from '@acpaas-ui/ngx-examples/logo';
 
 export const ExamplesModules = [
-	AgendaExamplesModule,
+
 	AvatarExamplesModule,
 	CalendarExamplesModule,
 	LogoExamplesModule,
--- styleguide/app/examples.routes.ts
+++ styleguide/app/examples.routes.ts
@@ -1,10 +1,9 @@
-import { AGENDA_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/agenda';
+
 import { AVATAR_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/avatar';
 import { CALENDAR_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/calendar';
 import { LOGO_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/logo';
 
 export const EXAMPLES_ROUTES = [
-	{ path: 'agenda', children: AGENDA_EXAMPLES_ROUTES },
 	{ path: 'avatar', children: AVATAR_EXAMPLES_ROUTES },
 	{ path: 'calendar', children: CALENDAR_EXAMPLES_ROUTES },
 	{ path: 'logo', children: LOGO_EXAMPLES_ROUTES },
